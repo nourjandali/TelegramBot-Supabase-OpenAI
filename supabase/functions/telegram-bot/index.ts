201ddce9441314f6b2d20832d7501b42c60db5cb
Nour Jandali <nourmshjan@gmail.com>
2023-10-02 21:20:52 +0300

    add onVoice message transcription

--- supabase/functions/telegram-bot/index.ts
+++ supabase/functions/telegram-bot/index.ts
@@ -45,6 +45,65 @@ bot.on("message:text", async (ctx) => {
 }
 );
 
+// on voice message.
+bot.on("message:voice", async (ctx) => {
+  try {
+    const voiceId = ctx.message.voice!.file_id;
+    const voiceInfo = await bot.api.getFile(voiceId);
+    const fileLink = `https://api.telegram.org/file/bot${
+      Deno.env.get("BOT_TOKEN")
+    }/${voiceInfo.file_path}`;
+    const fileResponse = await fetch(fileLink);
+    if (!fileResponse.ok) {
+      return ctx.reply("Error fetching file");
+    }
+    const fileBuffer = await fileResponse.arrayBuffer();
+    const file = new File([fileBuffer], voiceId, {
+      type: "audio/ogg",
+    });
+
+    // transcribe voice message.
+    await ctx.reply("Transcribing...");
+    const transcribe = await openai.createTranscription({
+      file: file,
+      model: "whisper-1",
+    });
+
+    // generate post.
+    await ctx.reply("Generating Post...");
+    const chat_completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            `You are a content repurposing professional, you take a transcribed voice message and you rewrite it into compelling content to instagram ad script. Only include the script. Remember the best instagram ads include the following elements:
+            1. Keep it simple and short. Stick to plain text.
+            2. Add emojis to your posts where appropriate.
+            3. Write a killer headline.
+            4. Open with a story where appropriate.
+            5. Break up walls of text.
+            6. Give specific instructions and unique insights.
+            7. Always end by asking a question.
+            8. Bring a new, unique angle where possible. Don't be afraid of a little controversy.
+            9. Brevity is key.
+            Think about whether your post makes sense as a whole, before you start writing.`,
+        },
+        {
+          role: "user",
+          content: transcribe.text,
+        },
+      ],
+    });
+    await ctx.reply(
+      chat_completion?.choices[0]?.message?.content || "No response",
+    );
+  } catch (err) {
+    console.error(err);
+    await ctx.reply("Error");
+  }
+});
+
 
 // initialize 'handleUpdate' function for webhook callbacks with 'bot' using 'std/http'.
 const handleUpdate = webhookCallback(bot, 'std/http')
